2ecf969928a40ba2c592f036b80b2586e876a88b
HoaiThuong <buithihoaithuong301@gmail.com>
2020-10-13 17:38:23 +0700

    them add ben services

--- WebForum/Models/BaiVietViewModels.cs
+++ WebForum/Models/BaiVietViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using WebForum.Data;
@@ -8,6 +9,7 @@ namespace WebForum.Models
     public class ListBaiViet
     {
         public int? ID { get; set; }
+        public int? IDLoaiBaiViet { get; set; }
         public string TenBaiViet { get; set; }
         public string linkIMG { get; set; }
         public int? countCmt { get; set; }
@@ -16,9 +18,16 @@ namespace WebForum.Models
         public string TenNguoiDang { get; set; }
         public DateTime? NgayDang { get; set; }
     }
-    public class AddBaiViet : BaiViet
+    public class AddBaiViet
     {
-
+        public int? ID { get; set; }
+        [Required(ErrorMessage = "Vui long chon danh muc bai viet")]
+        public int? IDLoaiBaiViet { get; set; }
+        [Required(ErrorMessage = "Vui long nhap tieu de bai viet")]
+        public string TenBaiViet { get; set; }
+        public string linkIMG { get; set; }
+        [Required(ErrorMessage = "Vui long nhap noi dung bai viet")]
+        public string NoiDung { get; set; }
     }
     
 }
--- WebForum/Services/BaiVietService.cs
+++ WebForum/Services/BaiVietService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using WebForum.Models;
 using WebForum.Data;
-
+using System.Net;
 
 namespace WebForum.Services
 {
@@ -16,7 +16,7 @@ namespace WebForum.Services
         public List<ListBaiViet> GetDanhSachBaiViet()
         {
             db = new DB_ForumEntities();
-            
+
             var lst = db.BaiViets.Select(m => new ListBaiViet
             {
                 ID = m.ID,
@@ -34,7 +34,38 @@ namespace WebForum.Services
         #endregion
 
         #region Add Bai Viet
-
+        public int AddEditBaiViet(AddBaiViet model)
+        {
+            
+            int kq = 0;
+            bool isAdd = true;
+            BaiViet baiviet = new BaiViet();
+            baiviet.ID = model.ID.Value;
+            baiviet.IDLoaiBaiViet = model.IDLoaiBaiViet;
+            baiviet.TieuDe = model.TenBaiViet;
+            baiviet.NoiDung = model.NoiDung;
+            baiviet.linkIMG = model.linkIMG;
+            if (model.ID==null)
+            {
+                isAdd = false;
+                baiviet.isCapNhat = true;
+                baiviet.NgayCapNhat = DateTime.Now;
+            }
+            else
+            {
+                baiviet.IDNguoiTao = null;// nua cap nhat sau
+                baiviet.NgayTao = DateTime.Now;
+            }
+            using (var db = new DB_ForumEntities())
+            {
+                if(isAdd)
+                {
+                    db.BaiViets.Add(baiviet);
+                }
+                db.SaveChanges();
+            }
+            return kq;
+        }
         #endregion
     }
 }
